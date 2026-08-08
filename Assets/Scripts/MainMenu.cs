d4a13b54cab09d770b8b7baada8a1f44b3f9dfae
Julaly <mtm296185@gmail.com>
2026-08-08 12:02:51 +0700

    Bug fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Start()
+    {
+        SceneManager.LoadSceneAsync("Scene01", LoadSceneMode.Additive);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Scene01");
